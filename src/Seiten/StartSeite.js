8e32b78eaeaf81681c641330e95ae973ca2aab78
Laieb786 <laieb.ahmed@gmail.com>
2023-06-03 18:28:52 +0200

    Aufbau einer Grundlegenden Struktur: Komponenten- und Seiten-Verzeichnis

---
+++ src/Seiten/StartSeite.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Hello World!
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
